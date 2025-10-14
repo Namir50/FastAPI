b9f83b4cb2fa05dee6adf0ef4a0d43758a8423bf
Namir50 <namirsayyed50@gmail.com>
2025-10-14 06:55:52 -0700

    query patameter with pydantic

--- query parameter/main3.py
+++ query parameter/main3.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from enum import Enum
 from pydantic_schema1 import GenreURLChoices,Band
+from typing import Optional
 
 app = FastAPI()
 
@@ -11,9 +12,21 @@ BANDS = [
     {'id': 4, 'name': 'Wu-Tang Clan', 'genre': 'Hip-Hop'},
 ]
 
+#without pydantic
+@app.get('/basicbands')
+async def basicbands(genre: Optional[str]=None):
+    if genre:
+        return[
+        b for b in BANDS if b['genre'].lower() == genre.lower()
+        ]
+    return BANDS
+
+#query parameter lets you use the same endpoint for multiple behaviors, depending on what parameters are passed
+#with pydantic
 @app.get('/bands')
-async def bands(genre:GenreURLChoices) -> list[Band]:
+async def bands(genre:GenreURLChoices | None = None) -> list[Band]:
     if genre:
         return [
-            Band(**b) for b in BANDS if b['genre'] == genre.value
+            Band(**b) for b in BANDS if b['genre'].lower() == genre.value
         ]
+    return [Band(**b) for b in BANDS]
