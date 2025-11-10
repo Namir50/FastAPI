a2fbe3c6bdb940a237715c519c95dcef8bca433b
Namir50 <namirsayyed50@gmail.com>
2025-11-10 05:54:28 -0800

    annotated type for verification

---
+++ annotated_type_for_validation_and_metadata/pydantic_schema2.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+from datetime import date
+from enum import Enum
+
+class GenreURLChoices(Enum):  
+    Rock= 'rock'
+    Electronic = 'electronic'
+    Classical = 'classical'
+    HipHop = 'hip-hop'
+
+
+class Album(BaseModel):
+    title: str
+    release_date : date
+
+class BandBase(BaseModel):
+    name: str
+    genre: str
+    albums: list[Album] = []
+
+class BandCreate(BandBase):
+    pass
+
+class BandwithID(BandBase):
+    id: int
---
+++ annotated_type_for_validation_and_metadata/test.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI,HTTPException
+from pydantic_schema2 import BandBase,BaseModel,BandCreate,BandwithID,GenreURLChoices
+from typing import Annotated,List
+
+BANDS = [
+    {'id': 1, 'name': 'The Kinks', 'genre': 'Rock'},
+    {'id': 2, 'name': 'Aphex Twin', 'genre': 'Electronic','albums':[
+        {'title':'Mater of reality','release_date': '1971-01-24'}
+    ]},
+    {'id': 3, 'name': 'Slowdive', 'genre': 'Classical'},
+    {'id': 4, 'name': 'Wu-Tang Clan', 'genre': 'Hip-Hop'},
+]
+
+app = FastAPI()
+
+@app.get('/bands')
+async def bands(
+    genre: GenreURLChoices | None = None,
+    has_albums: bool = False
+) -> List[BandwithID]:
+    band_list = [BandwithID(**b) for b in BANDS]
+
+    if genre:
+        band_list = [
+            b for b in band_list if b.genre.lower() == genre.value
+        ]
+    if has_albums:
+        band_list = [
+            b for b in band_list
+            if len(getattr(b, 'albums', [])) > 0
+        ]
+    
+    return band_list
+
+@app.get('/band/{band_id}')
+async def band(band_id: int) ->BandwithID:
+    band = next((BandwithID(**b) for b in BANDS if b['id']== band_id),None)
+    if band is None:
+        return HTTPException(status_code=404, detail=f'Band with id {band_id} not found')
+    
+@app.post('/bands')
+async def create_band(band_data: BandCreate) -> BandwithID:
+    id = (max(b['id'] for b in BANDS)+1) if BANDS else 1
+    band = BandwithID(id = id, **band_data.model_dump()).model_dump()
+    BANDS.append(band)
+    return band
