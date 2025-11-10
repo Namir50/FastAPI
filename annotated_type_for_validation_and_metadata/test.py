38b6df5a7bd6ad31923243ca5d571f3296869969
Namir50 <namirsayyed50@gmail.com>
2025-11-10 06:15:04 -0800

    annoated with query parameter

--- annotated_type_for_validation_and_metadata/test.py
+++ annotated_type_for_validation_and_metadata/test.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI,HTTPException
-from pydantic_schema2 import BandBase,BaseModel,BandCreate,BandwithID,GenreURLChoices
-from typing import Annotated,List
+from fastapi import FastAPI,HTTPException,Path, Query
+from pydantic_schema2 import BandBase, BandCreate, BandwithID, GenreURLChoices
+from typing import List,Annotated
 
 BANDS = [
     {'id': 1, 'name': 'The Kinks', 'genre': 'Rock'},
@@ -13,31 +13,28 @@ BANDS = [
 
 app = FastAPI()
 
-@app.get('/bands')
+@app.get("/bands")
 async def bands(
     genre: GenreURLChoices | None = None,
-    has_albums: bool = False
+    q: Annotated[str, Query(max_length=10)] = None,
 ) -> List[BandwithID]:
     band_list = [BandwithID(**b) for b in BANDS]
 
-    if genre:
-        band_list = [
-            b for b in band_list if b.genre.lower() == genre.value
-        ]
-    if has_albums:
-        band_list = [
-            b for b in band_list
-            if len(getattr(b, 'albums', [])) > 0
-        ]
-    
+    if genre is not None:
+        band_list = [b for b in band_list if b.genre.lower() == genre.value.lower()]
+
+    if q:
+        band_list = [b for b in band_list if q.lower() in b.name.lower()]
+
     return band_list
 
 @app.get('/band/{band_id}')
-async def band(band_id: int) ->BandwithID:
-    band = next((BandwithID(**b) for b in BANDS if b['id']== band_id),None)
+async def band(band_id: Annotated[int, Path(title='The band ID')]) -> BandwithID:
+    band = next((BandwithID(**b) for b in BANDS if b['id'] == band_id),None)
     if band is None:
         return HTTPException(status_code=404, detail=f'Band with id {band_id} not found')
-    
+
+#post request
 @app.post('/bands')
 async def create_band(band_data: BandCreate) -> BandwithID:
     id = (max(b['id'] for b in BANDS)+1) if BANDS else 1
