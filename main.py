4bf5eef9abef84d2e8a53d3f7b664d48bf368791
Namir50 <namirsayyed50@gmail.com>
2025-12-28 00:20:06 -0800

    fastapi start

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
-async def index() ->dict[str,str]:  #output datatype is used for data type verification or checking
-    return {'hello':'world'}
-
-@app.get('/about')
-async def about() -> str:
-    return 'An exceptional company'
+@app.get("/")
+async def root():
+    return {"message":"Hello world"}
