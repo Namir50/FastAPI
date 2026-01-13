36f0ad884e81a2ae34ce8d96435498b287dc8097
Namir50 <namirsayyed50@gmail.com>
2026-01-13 19:03:30 +0530

    storing post in memory

--- main.py
+++ main.py
@@ -11,13 +11,17 @@ class Post(BaseModel):
     published: bool = True #if the user doesn't provide this value it will default to true
     rating: Optional[int] = None #if the user doesn't provide this value it will default to None
 
+#story the data in the memory just for now before moving to database
+my_post = [{"id":1,"title": "title of post 1", "content": "content of post 1", "published": True}, 
+        {"id":2,"title": "title of post 2", "content": "content of post 2", "published": False}]
+
 @app.get("/")
 def root():
     return {"message":"welcome to my api"}
 
 @app.get("/posts")
 def get_posts():
-    return {"data":"this is your post"}
+    return {"data":my_post}
 
 @app.post("/posts")
 def create_post(post: Post):
